3de63a67acc19bf291b8ab731c02518d9764b552
kalvian <kalvianhajar@gmail.com>
2017-05-13 14:52:41 +0700

    contoh linear layout orientasi horizontal dan vertical

---
+++ app/src/main/java/id/kalviansofian/contohlinearlayout/MainActivity.java
@@ -0,0 +1,13 @@
+package id.kalviansofian.contohlinearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
